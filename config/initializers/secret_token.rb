afb500effc45aac5d42953f14675bcdd3b63be91
Jan-Willem van der Meer <mail@jewilmeer.nl>
2012-08-27 22:34:48 +0200

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SheDoesApi::Application.config.secret_token = '0acf010c039bfd1d96d6fe34f14dea8494fba9cddd8796062078483069f545e3965103fbe439ffbf5ac6a3664e9a8771f85b0d97fe0486c2493f393f0135f542'
